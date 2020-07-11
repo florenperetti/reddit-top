21b0fd2c2ad393595a94a77f12192f3a21c0bdac
Florencia Peretti <florenperetti@gmail.com>
2020-07-11 18:59:54 -0300

    drawer styles

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="drawer">
-    <div>Top posts</div>
-    <div>
+  <div class="drawer">
+    <div class="drawer__header">Top posts</div>
+    <div class="drawer__scroller">
 
     </div>
-    <div>Dismiss All</div>
+    <div class="drawer__footer">Dismiss All</div>
   </div>
 </template>
 
@@ -20,10 +20,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#drawer {
+.drawer {
   background-color: #212121;
   color: white;
-  padding: 16px;
+  display: flex;
+  flex-direction: column;
   height: 100%;
+
+  &__header,
+  &__footer {
+    height: 26px;
+    text-align: center;
+    padding: 8px;
+    flex-grow: 0;
+  }
+
+  &__scroller {
+    flex-grow: 1;
+    overflow-y: auto;
+  }
 }
 </style>

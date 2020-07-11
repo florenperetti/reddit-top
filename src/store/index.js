fb3f8aa8b8776621324ff40078df1f0fc2e60da8
Florencia Peretti <florenperetti@gmail.com>
2020-07-11 20:49:10 -0300

    add plugin to persist state in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import posts from './modules/posts'
+// import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -8,4 +9,5 @@ export default new Vuex.Store({
   modules: {
     posts
   }
+  // plugins: [createPersistedState()]
 })
